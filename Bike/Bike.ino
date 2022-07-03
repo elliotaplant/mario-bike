647b44097ef20a8a9a70fc3c3a215191e3e5857f
Elliot Plant <elliotaplant@gmail.com>
2022-07-02 18:47:42 -0700

    Safety

--- Bike/Bike.ino
+++ Bike/Bike.ino
@@ -4,14 +4,25 @@ https://github.com/elliotaplant/mario-bike
 Turn your bike into a Mario Kart controller
 */
 
+#include "InternalTemperature.h"
+
 const int numButtons = 12;
 const int ANALOG_PIN = 0;
+const int scaleFactor = 2;
+const int limit = 1024;
+const int ledPin = 13;
+
 
 void setup()
 {
   Serial.begin(9600);
 
-  Serial.print("Beginning Mario Bike");
+//  Serial.println("Beginning Mario Bike");
+  Serial.print("hallValue");
+  Serial.print("\t");
+  Serial.print("trailingAverageValue");
+  Serial.print("\t");
+  Serial.println("difference");
   // configure the joystick to manual send mode.  This gives precise
   // control over when the computer receives updates, but it does
   // require you to manually call Joystick.send_now().
@@ -19,15 +30,27 @@ void setup()
   for (int i = 0; i < numButtons; i++) {
     pinMode(i, INPUT_PULLUP);
   }
-  Serial.println("Begin Complete Joystick Test");
+
+  // Prepare the LED pin for output
+  pinMode(ledPin, OUTPUT);
 }
 
 byte allButtons[numButtons];
 byte prevButtons[numButtons];
 
+int leftJoystick = 1023;
+int centerJoystick = 512;
+int rightJoystick = 0;
+
 void loop()
 {
-  Joystick.X(analogRead(ANALOG_PIN));
+  int analogValue = analogRead(ANALOG_PIN);
+  int interpolatedValue = interpolateJoystick(analogValue);
+  
+  Joystick.X(interpolatedValue);
+
+  int msPerCycle = getSpeed();
+  Serial.println(msPerCycle, DEC);
   
    // read digital pins and use them for the buttons
   for (int i = 0; i < numButtons; i++) {
@@ -50,18 +73,54 @@ void loop()
   // check to see if any button changed since last time
   boolean anyChange = false;
   for (int i=0; i<numButtons; i++) {
-    if (allButtons[i] != prevButtons[i]) anyChange = true;
+    if (allButtons[i] != prevButtons[i]) {
+      anyChange = true;
+    }
     prevButtons[i] = allButtons[i];
   }
   
   // if any button changed, print them to the serial monitor
   if (anyChange) {
-    Serial.print("Buttons: ");
-    for (int i=0; i<numButtons; i++) {
-      Serial.print(allButtons[i], DEC);
-    }
-    Serial.println();
+    stateChangeButtons(analogValue); 
   }
 
   delay(5);
+}
+
+int interpolateJoystick(int analogValue) {
+  if (analogValue > leftJoystick) {
+    return 1024;
+  } else if (analogValue > centerJoystick) {
+    return ((512 * (analogValue - centerJoystick)) / (1024 - centerJoystick)) + 512;
+  } else if (analogValue > rightJoystick) {
+    return (512 * (analogValue - rightJoystick)) / (centerJoystick - rightJoystick);
+  }
+  return 0;
+}
+
+void stateChangeButtons(int joystickReading) {
+  if (allButtons[4]) {
+    if (allButtons[5]) {
+      leftJoystick = joystickReading;
+      Serial.print(joystickReading, DEC);
+      Serial.println(" Lefting");
+      blink();
+    } else if (allButtons[2]) {
+      Serial.print(joystickReading, DEC);
+      Serial.println(" Centering");
+      centerJoystick = joystickReading;
+      blink();
+    } else if (allButtons[3]) {
+      Serial.print(joystickReading, DEC);
+      Serial.println(" Righting");
+      rightJoystick = joystickReading;
+      blink();
+    }
+  }
+}
+
+void blink() {
+  digitalWrite(ledPin, HIGH);   // set the LED on
+  delay(1000);                  // wait for a second
+  digitalWrite(ledPin, LOW);    // set the LED off
 }
---
+++ Bike/Speed.ino
@@ -0,0 +1,51 @@
+/*
+Speed sensor
+- Sets up the hall effect sensor
+- Establishes a "base" range for the hall effect sensor with a trailing average
+- Determines if a given "tick" is touching the magnet 
+- Debounces magent touches
+- Determines the RPM of the rider
+*/
+
+const int HALL_EFFECT_SENSOR_PIN = 16;
+const int NUM_SAMPLES = 20;
+const int CHANGE_THRESHOLD = 5;
+const int ROTATION_DEBOUNCE_MS = 200;
+const int RPM_SMOOTHING_CYCLES = 3;
+
+int prevReadings[NUM_SAMPLES];
+int readingIndex = 0;
+int currentRpm = 0;
+int trailingAverageSum = 0;
+time_t lastPass = 0;
+int prevRotationMs[RPM_SMOOTHING_CYCLES];
+int currentMsPerRotationSum = 0;
+int smoothingIndex = 0;
+
+int getSpeed() {
+  int hallValue = analogRead(HALL_EFFECT_SENSOR_PIN);
+
+  // Add the new reading to the trailing sum before putting it in the ring buffer
+  trailingAverageSum += (hallValue - prevReadings[readingIndex]);
+  prevReadings[readingIndex] = hallValue;
+  readingIndex = (readingIndex + 1) % NUM_SAMPLES;
+
+  int trailingAverageValue = trailingAverageSum / NUM_SAMPLES;
+
+  int difference = abs(hallValue - trailingAverageValue);
+
+  if (difference > CHANGE_THRESHOLD) {
+    Serial.println("MAGNETRON");
+    time_t thisPass = now();
+    time_t msSince = thisPass - lastPass;
+    
+    if (msSince > ROTATION_DEBOUNCE_MS) {
+      lastPass = thisPass;
+      currentMsPerRotationSum += (msSince - prevRotationMs[smoothingIndex]);
+      prevRotationMs[smoothingIndex] = msSince;
+      smoothingIndex = (smoothingIndex + 1) % RPM_SMOOTHING_CYCLES;
+    }
+
+    return currentMsPerRotationSum / RPM_SMOOTHING_CYCLES;
+  }
+}
